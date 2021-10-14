65458532a1ae0e2b82a742d9d79bf804cf65f711
Bhavuk <bhavuk.11.sharma@gmail.com>
2021-10-14 21:30:06 +0530

    feat: geocode error example

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,35 +1,40 @@
-/*
-// Example of Async Calls
-console.log('Starting')
+const request = require('postman-request');
+const { features } = require('process');
+// const url = 'http://api.weatherstack.com/current?access_key=8869458679db594bceb778d309b4d83e&query=31.1234,-100.1212&units=f'
 
-setTimeout(()=>{
-   console.log('2 SECOND Timer')
-}, 2000)
+// request({url: url, json: true}, (error, response)=>{
+//     // console.log(response)
+//     // const data = JSON.parse(response.body)
+//     // console.log(data.current.observation_time)
+//     if(error){
+//         console.log('Unable to connect to weather service')
+//     }else if(response.body.error){
+//         console.log(response.body.error.info)
+//     }else{
+//         // const longi = response.body.features[0].center[0]
+//         // const lati  = response.body.features[0].center[1]
+//         // console.log(longi +', '+ lati)
+//         console.log('It is currently '+response.body.current.temperature+' degrees Fahrenheit out there and there are '+
+//                      response.body.current.precip+'\% chances of rain. Condition is : '+response.body.current.weather_descriptions[0])
+//     }
+// })
 
-setTimeout(()=>{
-    console.log('0 SECOND Timer')
- }, 0)
 
- 
-console.log('Stopping')
-
-http://api.weatherstack.com/current?access_key=8869458679db594bceb778d309b4d83e&query=31.1234,-100.1212
-*/
-const request = require('postman-request')
-const url = 'http://api.weatherstack.com/current?access_key=8869458679db594bceb778d309b4d83e&query=31.1347,-100.1123&units=f'
 // const place = 'Shimla'
-// const geoCodeURL  = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Chandigarh.json?access_token=pk.eyJ1Ijoia2VuamltYXBib3giLCJhIjoiY2t1cGl6OGdxMThvbDJ2cDFwNngyanNmZyJ9.HjXFdRkpXvWuHR3eFGP59g&limit=1'
-request({url: url, json: true}, (error, response)=>{
-    // console.log(response)
-    // const data = JSON.parse(response.body)
-    // console.log(data.current.observation_time)
+const geoCodeURL  = 'https://api.mapbox.com/geocoding/v5/mapbox.places/cupertino.json?access_token=pk.eyJ1Ijoia2VuamltYXBib3giLCJhIjoiY2t1cGl6OGdxMThvbDJ2cDFwNngyanNmZyJ9.HjXFdRkpXvWuHR3eFGP59g&limit=1'
+
+request({url: geoCodeURL, json: true}, (error, response)=>{
     if(error){
-        console.log('Could not reach weather service')
+        console.log('Unable to connect to weather service')
+    }else if(response.body.message || response.body.features.length === 0){
+        if(response.body.message)
+            console.log(response.body.message)
+        else if(response.body.features.length === 0)
+            console.log('No such city exist!')
     }else{
-        // const longi = response.body.features[0].center[0]
-        // const lati  = response.body.features[0].center[1]
-        // console.log(longi +', '+ lati)
-        console.log('It is currently '+response.body.current.temperature+' degrees Fahrenheit out there and there are '+
-                     response.body.current.precip+'\% chances of rain. Condition is : '+response.body.current.weather_descriptions[0])
+        const longi = response.body.features[0].center[0]
+        const lati  = response.body.features[0].center[1]
+        console.log(longi +', '+ lati)
     }
-})
+});
+
