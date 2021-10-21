c291cecedfbde535c1e9a0779837aeee250523cc
Bhavuk <bhavuk.11.sharma@gmail.com>
2021-10-21 18:51:03 +0530

    feat: partials and template engine/views using hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,48 @@
 const path = require('path')
 const express = require('express')
-
+const hbs = require('hbs')
 
 // console.log(__dirname)
 // console.log(__filename)
 // console.log(path.join(__dirname, '../public','index.html'))
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath) //by default express expects the templates to be in 'views' directory of the project.
+
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    res.render('index',{
+        title: 'Weather',
+        name: 'Bhavuk'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Bhavuk'
+    })
+})
+
+app.get('/help', (req, res) =>{
+    res.render('help', {
+        title: 'Help',
+        name: 'Bhavuk',
+        helpText: 'We are in a bit of a fix 😬 , HELP US! 🙏 ',
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
