1dd19c1ceb4c224d4f1d93aa9fe78364e5931944
Bhavuk <bhavuk.11.sharma@gmail.com>
2021-11-05 13:44:38 +0530

    mongoose models playground promises

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 const User = mongoose.model('User', {
@@ -7,13 +9,40 @@ const User = mongoose.model('User', {
     },
     age: {
         type: Number
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value))
+                throw new Error('Invalid e-mail')
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value)
+        {
+            // let pwd = value.trim().toLowerCase()
+            // if(value.length < 6)
+            //     throw new Error('Password must be greater than 6 characters')
+            if(value.toLowerCase().includes('password')){
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     }
 })
 
 /*
 const me = new User({
-    name: 'Bhavuk', 
-    age: 27
+    name: 'Ravi Kishan', 
+    age: 43,
+    email: 'ravi@gmx.com',
+    password: 'NavalRavicant'
 })
 
 me.save().then( () => {
@@ -25,20 +54,23 @@ me.save().then( () => {
 
 const Task = mongoose.model('Task', {
     desciption: {
-        type: String
+        type: String,
+        trim: true,
+        required: true,
+
     },
     completed:{
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
 const mytask = new Task({
-    desciption: "Get a plant",
-    completed: false
+    desciption: "Get a house     ",
 })
 
 mytask.save().then( () => {
-    console.log(task)
+    console.log(mytask)
 }).catch((er) => {
     console.log(er)
 })
